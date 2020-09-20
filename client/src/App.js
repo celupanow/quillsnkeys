85530edca5fc3f1259b1d31d4dad26470ae83e21
Lady Clu <celupanow@gmail.com>
2020-09-20 14:34:18 -0600

    updating basic pages and npm packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <Router>
       <div>
         <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
       </div>
     </Router>
 
