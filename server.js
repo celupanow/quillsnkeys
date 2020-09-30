3fb6a8910ac46a103200e56976f5f556f9fa714e
Lady Clu <celupanow@gmail.com>
2020-09-29 21:55:21 -0600

    application now connected to cluster0 on MongoDB Atlas

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
+const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 3001;
 
 //define middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //serve up static assets
 if (process.env.NODE_ENV === "production") {
@@ -14,12 +20,15 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //add routes
-app.use(routes);
+// app.use(routes);
 
 //connect to the Mongo DB
-mongoose.connect(process.env.MONBODB_URI || "mongodb://localhost/quillsnkeys");
+mongoose
+.connect(MONGO_URI, { useNewurlParser: true })
+.then(() => console.log("Mongo Connection successful"))
+.catch(err => console.log("err"));
 
 //start the server
 app.listen(PORT, function() {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    console.log(`🌎  ==> Server now listening on PORT ${PORT}!`);
 });
