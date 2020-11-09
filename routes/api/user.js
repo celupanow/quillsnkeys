c1114210ced0217780fb7f085b56288911a3b5a4
Lady Clu <celupanow@gmail.com>
2020-11-08 17:32:38 -0700

    starting on routes

---
+++ routes/api/user.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require ("bcryptjs");
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.SECRET;
+const validateSignUpInput = require("../../validation/signup");
+const validateLoginInput = require("../../validation/login");
+const User = require("../../models/User");
