50e7a6d6ddecdaf89917c368e4521eb6968b8461
Lady Clu <celupanow@gmail.com>
2020-12-09 19:50:14 -0700

    updating passport code

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const routes = require("./routes");
 const app = express();
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 3001;
@@ -20,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //add routes
-app.use(routes);
+app.use("/api/users/", require("./routes/api/users"));
 
 //connect to the Mongo DB
 mongoose
