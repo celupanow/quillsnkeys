60adfa757f1c3c22680c3254b83c3c642f581082
Lady Clu <celupanow@gmail.com>
2021-01-01 20:16:07 -0700

    updating post routes

--- routes/api/post.js
+++ routes/api/post.js
@@ -10,7 +10,7 @@ router.get(
     "/",
     passport.authenticate("jwt", { session: false }),
     (req, res) => {
-       Post.find({ author: req.user.user_name })
+       Post.find({ author: req.user.username })
           .then(posts => res.status(200).json(posts))
           .catch(err =>
              res
@@ -24,6 +24,7 @@ router.get(
        .then(post => res.status(200).json(post))
        .catch(err => res.status(400).json({ id: "Error fetching post by id" }));
  });
+
  router.get("/author/:author", (req, res) => {
     Post.find({ author: req.params.author })
        .then(posts => res.status(200).json(posts))
@@ -33,4 +34,61 @@ router.get(
              .json({ author: "Error fetching posts of specific author" })
        );
  });
+
+ router.post(
+    "/create",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+       const author = req.user.username;
+       const post = req.body;
+       const { errors, invalid } = validatePostInput(post);
+       
+       if (!isValid) {
+          return res.status(400).json(errors);
+       }
+       post.author = author;
+       const newPost = new Post(post);
+       newPost
+         .save()
+         .then(doc => res.json(doc))
+         .catch(err => console.log({ create: "Error creating new post" }));
+    }
+ );
+
+ router.patch(
+   "/update/:id",
+   passport.authenticate("jwt", { session: false }),
+   (req, res) => {
+      const author = req.user.user_name;
+      const { errors, isValid } = validatePostInput(req.body);
+      if (!isValid) {
+         return res.status(400).json(errors);
+      }
+      const { title, body } = req.body;
+      Post.findOneAndUpdate(
+         { author, _id: req.params.id },
+         { $set: { title, body } },
+         { new: true }
+      )
+         .then(doc => res.status(200).json(doc))
+         .catch(err =>
+            res.status(400).json({ update: "Error updating existing post" })
+         );
+   }
+);
+
+router.delete(
+   "/delete/:id",
+   passport.authenticate("jwt", { session: false }),
+   (req, res) => {
+      const author = req.user.user_name;
+      Post.findOneAndDelete({ author, _id: req.params.id })
+         .then(doc => res.status(200).json(doc))
+         .catch(err =>
+            res.status(400).json({ delete: "Error deleting a post" })
+         );
+   }
+);
+
+module.exports = router;
  
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const passport = require("passport");
 const app = express();
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 3001;
@@ -16,16 +17,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //serve up static assets
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 }
 
 //add routes
+app.use(passport.initialize());
+require("./middleware/passport")(passport);
 app.use("/api/users/", require("./routes/api/users"));
+app.use("/api/post/", require("./routes/api/post"));
+
 
 //connect to the Mongo DB
 mongoose
 .connect(MONGO_URI, { useNewurlParser: true })
 .then(() => console.log("Mongo Connection successful"))
 .catch(err => console.log("err"));
+
+mongoose.set("useFindAndModify", false);
+mongoose.Promise = global.Promise;
+
+
 
 //start the server
 app.listen(PORT, function() {
